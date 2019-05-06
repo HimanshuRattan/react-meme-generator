5484776bec72b2e8f1edc82976981c1cbf3a71da
HimanshuRattan <35609063+HimanshuRattan@users.noreply.github.com>
2019-05-06 21:04:17 +0530

    init

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header.js';
+import MemeGenerator from './MemeGenerator';
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<MemeGenerator />
+		</div>
+	);
+}
+export default App;
